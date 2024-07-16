906c54bf83ec7c3380f2c00639c203201087d98b
CGAJAY <jmugendi737@gmail.com>
2024-07-16 21:45:06 +0300

    Added pseudocode and some basic code

--- script.js
+++ script.js
@@ -1,50 +1,43 @@
-// If a number is autobiographical, then the number's digits describe itself.
-// The first digit is the amount of times the digit 0 appears,
-//  the second digit is the amount of times the digit 1 appears,
-//  the third digit is the amount of times the digit 2 appears, etc.
-//  This repeats for all digits of the number.
-//  The number 6210001000 is autobiographical,
-//  because it has six 0's, two 1's, one 2, zero 3's, zero 4's,
-//  zero 5's, one 6, zero 7's, zero 8's, and zero 9's.
+let test = document.getElementById("test");
+function checkNumberOfDigits() {
+	// Grab the number enterred
+	let num = document.getElementById("number-cont").value;
 
-// Create a function that takes an integer n and returns whether or not n is an autobiographical number
-
-// first function, create function checkNumberOfDigits
-// check the number of digits in the number
-// if the number of digits is >= 4 and <= 10 return that number
-// if the number of digits is < 4 and > 10 return false
-
-// second function,
-// create a new variable  'newNumber' to save output of checkNumberOfDigits
-// if the value of newNumber is not equals to false do the following
-// Loop through the newNumber
-// nest another loop
-// in the nested loop check if the index is equal to the element at every index
-// If true have a temporary container to register how many times this is true
-// after the nested loop ends compare the value of the temporary container to the value of the newNumber
-// at the current index in the outer loop
-// if this is false terminate the loop
-// if true continue with the loop
-// Repeat the same steps until the we check for all the digits
-
-let newNumber = "12101";
-let count = 0;
-let state = false;
-
-for (let i = 0; i < newNumber.length; i++) {
-	// for all the  elements loop to see if they are equal to the number of times their respective index appears
-	for (let j = 0; j < newNumber.length; j++) {
-		if (newNumber[j] == i) {
-			count++;
-		}
+	// Check the length of the number enterred
+	if (num.length >= 4 && num.length <= 10) {
+		return num;
 	}
-	if (newNumber[i] == count) {
+	// return false;
+}
+
+function autobiographical() {
+	// save the output of checkNumberOfDigits in a variable
+	let numba = checkNumberOfDigits();
+	console.log(numba);
+
+	// create variable to hold the number of times a digit appears
+	let count = 0;
+
+	//loop thru the elements in the number string
+	for (let i = 0; i < numba.length; i++) {
+		//loop again thru the elements in the numba string
+		for (let j = 0; j < numba.length; j++) {
+			// Check if the number at index j in the second loop is equal to the outer index
+			if (numba[j] == i) {
+				// if the digit in the first loop is equal to any other digit in the numba increment count
+				count++;
+			}
+		}
+		// Check if the digit at that index is not equal to the count variable
+		// if true return false
+		if (numba[i] != count) {
+			console.log("Not autobiographical");
+			return false;
+		}
+		// If false assign zero to the count and continue with the loop
 		count = 0;
-		state = true;
-		console.log(state);
-	} else {
-		state = false;
-		console.log(state);
-		return state;
+		console.log("autobiographical");
+		return true;
 	}
 }
+test.addEventListener("click", autobiographical);
