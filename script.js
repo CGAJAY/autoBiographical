a0c1c3e1ac5a3ba4035feb7d594e2d7d314e4147
CGAJAY <jmugendi737@gmail.com>
2024-07-16 22:47:56 +0300

    Rendered the output in the DOM

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const submitBtn = document.getElementById("submit");
+const container = document.querySelector(".container");
+const inputNum = document.getElementById("number-cont");
+// Create a result container
+const resultCont = document.createElement("div");
+resultCont.setAttribute("id", "result-cont");
+// Create a h2 to hold the results
+const result = document.createElement("h2");
+result.setAttribute("id", "result");
+
 function checkNumberOfDigits() {
 	// Grab the number enterred
 	let num = document.getElementById("number-cont").value;
@@ -7,12 +16,19 @@ function checkNumberOfDigits() {
 	if (num.length >= 4 && num.length <= 10) {
 		return num;
 	}
-	// return false;
+	return false;
 }
 
 function autobiographical() {
 	// save the output of checkNumberOfDigits in a variable
 	let numba = checkNumberOfDigits();
+	inputNum.value = "";
+
+	// Check if value of numba is  false
+	if (numba === false) {
+		// if false return -1
+		return -1;
+	}
 	console.log(numba);
 
 	// create variable to hold the number of times a digit appears
@@ -40,7 +56,28 @@ function autobiographical() {
 		return true;
 	}
 }
+
+function render() {
+	result.textContent = "";
+	// save the return value of autobiographical() in a variable
+	let mainOutput = autobiographical();
+
+	// have a condition to render different output respective to value in mainOutput
+	if (mainOutput === -1) {
+		result.textContent =
+			"Number should have 4 digits minimum and 10 digits maximum";
+	} else if (mainOutput === false) {
+		result.textContent = "Number is not autobiographical";
+	} else {
+		result.textContent = "Number is a autobiographical";
+	}
+	// Append result to the result container
+	resultCont.appendChild(result);
+	// Append result container to the main container
+	container.appendChild(resultCont);
+}
+
 submitBtn.addEventListener("click", (e) => {
 	e.preventDefault();
-	autobiographical();
+	render();
 });
